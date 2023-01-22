c097528b82e800562db2bdd970a426c164ed289a
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-01-23 00:35:37 +0400

    Covered validation result.

--- tests/unit/CountryVatFormatEmptinessValidatorTest.php
+++ tests/unit/CountryVatFormatEmptinessValidatorTest.php
@@ -12,4 +12,26 @@ class CountryVatFormatEmptinessValidatorTest extends CountryVatFormatValidatorTe
 
         $this->validator = new CountryVatFormatEmptinessValidator();
     }
+
+    /**
+     * @dataProvider getVatNumberProvidedData
+     */
+    public function testValidationResult(string $vatNumber, bool $isValid): void
+    {
+        $this->assertEquals($isValid, $this->validator->isValid($vatNumber));
+    }
+
+    public function getVatNumberProvidedData(): array
+    {
+        return [
+            'empty vat number' => [
+                'vatNumber' => '',
+                'isValid' => false,
+            ],
+            'not empty vat number' => [
+                'vatNumber' => '12313213',
+                'isValid' => true,
+            ],
+        ];
+    }
 }
