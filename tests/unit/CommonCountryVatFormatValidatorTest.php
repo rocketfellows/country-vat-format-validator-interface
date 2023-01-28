3aa68571b1e254c7640706d118912aed795cde6b
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-01-28 15:58:00 +0400

    Covered handle validation error.

--- tests/unit/CommonCountryVatFormatValidatorTest.php
+++ tests/unit/CommonCountryVatFormatValidatorTest.php
@@ -2,6 +2,11 @@
 
 namespace rocketfellows\CountryVatFormatValidatorInterface\tests\unit;
 
+use Error;
+use Exception;
+use rocketfellows\CountryVatFormatValidatorInterface\exceptions\CountryVatFormatValidationException;
+use Throwable;
+
 class CommonCountryVatFormatValidatorTest extends CountryVatFormatValidatorTest
 {
     private const IS_VALID_FORMAT_DEFAULT_TEST_VALUE = true;
@@ -11,6 +16,68 @@ class CommonCountryVatFormatValidatorTest extends CountryVatFormatValidatorTest
         parent::setUp();
 
         $this->validator = new CountryVatFormatValidatorMock(self::IS_VALID_FORMAT_DEFAULT_TEST_VALUE);
+    }
+
+    /**
+     * @dataProvider getValidationErrorProvidedData
+     */
+    public function testHandleValidationError(
+        string $vatNumber,
+        Throwable $thrownValidationException,
+        Exception $expectedException
+    ): void {
+        $this->validator = new CountryVatFormatValidatorMock(true, $thrownValidationException);
+
+        $this->expectExceptionObject($expectedException);
+
+        $this->validator->isValid($vatNumber);
+    }
+
+    public function getValidationErrorProvidedData(): array
+    {
+        $exceptionWithoutMessage = new Exception();
+        $exceptionWithMessage = new Exception('foo');
+        $errorWithoutMessage = new Error();
+        $errorWithMessage = new Error('bar');
+
+        return [
+            'thrown exception without' => [
+                'vatNumber' => 'foo',
+                'thrownValidationError' => $exceptionWithoutMessage,
+                'expectedException' => new CountryVatFormatValidationException(
+                    '',
+                    0,
+                    $exceptionWithoutMessage
+                ),
+            ],
+            'thrown exception with message' => [
+                'vatNumber' => 'foo',
+                'thrownValidationError' => $exceptionWithMessage,
+                'expectedException' => new CountryVatFormatValidationException(
+                    'foo',
+                    0,
+                    $exceptionWithMessage
+                ),
+            ],
+            'thrown error without message' => [
+                'vatNumber' => 'foo',
+                'thrownValidationError' => $errorWithoutMessage,
+                'expectedException' => new CountryVatFormatValidationException(
+                    '',
+                    0,
+                    $errorWithoutMessage
+                ),
+            ],
+            'thrown error with message' => [
+                'vatNumber' => 'foo',
+                'thrownValidationError' => $errorWithMessage,
+                'expectedException' => new CountryVatFormatValidationException(
+                    'bar',
+                    0,
+                    $errorWithMessage
+                ),
+            ],
+        ];
     }
 
     /**
