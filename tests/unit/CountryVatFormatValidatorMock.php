9425005bfe0e92b0d3cb4d82daf184bda9da00ca
Arslan Imamutdinov <ara.imamutdinov@gmail.com>
2023-01-28 15:48:12 +0400

    Added mock validator.

---
+++ tests/unit/CountryVatFormatValidatorMock.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace rocketfellows\CountryVatFormatValidatorInterface\tests\unit;
+
+use Throwable;
+
+class CountryVatFormatValidatorMock extends CountryVatFormatValidator
+{
+    private $isValidFormat;
+    private $validationError;
+
+    public function __construct(
+        bool $isValidFormat,
+        ?Throwable $validationError = null
+    ) {
+        $this->isValidFormat = $isValidFormat;
+        $this->validationError = $validationError;
+    }
+
+    protected function isValidFormat(string $vatNumber): bool
+    {
+        if (!is_null($this->validationError)) {
+            throw $this->validationError;
+        }
+
+        return $this->isValidFormat;
+    }
+}
